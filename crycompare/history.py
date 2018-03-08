1ec51c16e07a237b8cd9e9d30ddffa8f0fc065d2
Stefan Stojanovic <stefan.stojanovic@sbgenomics.com>
2018-03-08 17:06:00 +0100

    return data only

--- crycompare/history.py
+++ crycompare/history.py
@@ -54,6 +54,6 @@ def __get_url(url):
 		raw_data.raise_for_status()
 		return False
 	try:
-		return raw_data.json()
+		return raw_data.json()['Data']
 	except NameError:
 		raise ValueError('Cannot parse to json.')
